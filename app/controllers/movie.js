dd1bbb0125a6b26621e411b41b96fc21876b2e42
Tim Minkov <timothyminkov@gmail.com>
2014-08-21 20:19:10 -0700

    Posters working

--- app/controllers/movie.js
+++ app/controllers/movie.js
@@ -11,6 +11,22 @@ export default Ember.ObjectController.extend({
     }
   },
 
+  imdbImage: function() {
+    var id = this.get('id');
+    $.getJSON('https://api.themoviedb.org/3/find/' + id + '?api_key=227fe3bb4564a1ba35150061d97e905c&external_source=imdb_id').done(function(data) {
+      if (data.movie_results) {
+        var results = data.movie_results[0];
+        var imageLocation = 'http://image.tmdb.org/t/p/original/' + results.poster;
+        debugger;
+        this.set('imageLocation', imageLocation);
+      } else if (data.tv_results) {
+        var results = data.tv_results[0];
+        var imageLocation = 'http://image.tmdb.org/t/p/original/' + results.poster;
+        this.set('imageLocation', imageLocation);
+      }
+    });
+  },
+
   needMoreInfo: false,
 
   uncertainText: function() {
--- app/routes/movie.js
+++ app/routes/movie.js
@@ -2,7 +2,32 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function(params) {
-    return $.getJSON('http://www.omdbapi.com/?tomatoes=true&i=' + params.id).then(this.parseMovie);
+    return Ember.RSVP.hash({
+      movie: $.getJSON('http://www.omdbapi.com/?tomatoes=true&i=' + params.id).then(this.parseMovie),
+      poster: $.getJSON('https://api.themoviedb.org/3/find/' + params.id + '?api_key=227fe3bb4564a1ba35150061d97e905c&external_source=imdb_id').then(this.parsePoster),
+    });
+  },
+
+  setupController: function(controller, models) {
+    var movie = models.movie;
+    var poster = models.poster;
+
+    controller.set('content', movie);
+    controller.set('poster', poster);
+  },
+
+  parsePoster: function(data) {
+    if (data.movie_results) {
+      var results = data.movie_results[0];
+      var imageLocation = 'http://image.tmdb.org/t/p/original' + results.poster_path;
+      var obj = {imageLocation: imageLocation};
+      return obj;
+    } else if (data.tv_results) {
+      var results = data.tv_results[0];
+      var imageLocation = 'http://image.tmdb.org/t/p/original' + results.poster_path;
+      var obj = {imageLocation: imageLocation};
+      return obj;
+    }
   },
 
   parseMovie: function(data) {
